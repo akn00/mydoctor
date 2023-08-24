5ce63061237857690796318237859f39ce751d99
akn00 <aknikhil.k@gmail.com>
2023-08-24 17:39:28 +0530

    added swiper and started paging for specialities page

--- src/components/InfiniteBar/index.js
+++ src/components/InfiniteBar/index.js
@@ -1,10 +1,73 @@
-import "./infiniteBar.css"
+import React from "react";
+import "./infiniteBar.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; 
 
 const InfiniteBar = () => {
-    return ( 
+  const specialities = [
+    "Dementia",
+    "Depression",
+    "Diabetes",
+    "Diphtheria",
+    "Dyslexia",
+    "Obesity",
+    "Vertigo",
+    "Dementia",
+    "Depression",
+    "Diabetes",
+    "Diphtheria",
+    "Dyslexia",
+    "Obesity",
+    "Vertigo",
+    "Dementia",
+    "Depression",
+    "Diabetes",
+    "Diphtheria",
+    "Dyslexia",
+    "Obesity",
+    "Vertigo",
+    "Obesity",
+    "Vertigo",
+    "Dementia",
+    "Depression",
+    "Diabetes",
+    "Diphtheria",
+    "Dyslexia",
+    "Obesity",
+    "Vertigo"
+  ];
+
+  const breakpoints = {320: { slidesPerView: 3 },480: { slidesPerView: 4 },768: { slidesPerView: 5 },1024: { slidesPerView: 8 }};
+  return (
     <div className="infiniteBar">
+      <Swiper
+        spaceBetween={20}
+        pagination={false}
+        style={{display:"flex", marginLeft:0, marginRight:0, width:"100%"}}
+        // slidesPerView={{lg:10,md:10,sm:6,xs:4}}
+        breakpoints={breakpoints}
+        freeMode={true}
+      >
+        {specialities.map((speciality, index) => (
+          <SwiperSlide
+            key={index}
+            style={{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            // textAlign:"center",
+              width: "auto",
+              height:"1.75rem",
+              borderRadius: "15px",
+              padding:"0 10px"
+            }}
+          >
+            {speciality}
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-     );
-}
- 
+  );
+};
+
 export default InfiniteBar;
--- src/components/Pages/HomeDr/DrCard.js
+++ src/components/Pages/HomeDr/DrCard.js
@@ -1,19 +1,21 @@
 import React from 'react';
-// import "./CardSp.css"
-import dummyImg from "../../../assets/SpecialitiesDummyImg.svg"
+import "./DrCard.css"
+// import dummyImg from "../../../assets/SpecialitiesDummyImg.svg"
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography'; 
-
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import PersonIcon from '@mui/icons-material/Person';
+import Button from '@mui/material/Button';
 
 
 const DrCard = ({ doctor }) => {
-    const[hospital,setHospital]=React.useState();
-    const experience=Math.floor((doctor?.profile?.experienceMonths || [])/12)
+    const [hospital, setHospital] = React.useState();
+    const experience = Math.floor((doctor?.profile?.experienceMonths || []) / 12)
 
     React.useEffect(() => {
         let hospitalName = "Not Available";
-        
+
         if (doctor?.profile?.experience) {
             for (const exp of doctor.profile.experience) {
                 if (exp.toYear) {
@@ -24,41 +26,56 @@ const DrCard = ({ doctor }) => {
                 }
             }
         }
-        
+
         setHospital(hospitalName);
     }, [doctor]);
 
 
-    
+
     return (
-        <div className='cardSp'>
-            <Card variant="outlined">
-                <CardContent style={{ display: "flex", alignItems: "center", flexDirection: "column", gap: "2rem" }}>
-                    <img src={dummyImg} alt="card img" style={{ width: "100PX" }} />
+        <div className='cardDr'>
+            <Card variant="outlined" style={{height:"100%"}}>
+                <CardContent style={{ display: "flex", alignItems: "center", flexDirection: "column", gap: "2rem",padding: "1.8rem 1.2rem"}}>
+                    <div className='drCardMain'>
+                    
+                    <div className='drCardlogo'>
+                        <Box style={{ backgroundColor: "lightGrey", borderRadius: "50%" }}>
+                            <PersonIcon style={{ color: "white", height: "80px", width: "80px" }} />
+                        </Box>
+                    </div>
+
+                    <div className='drCardContent'>
+                        <div className='withoutButton'>
                     <Typography color="text.primary" style={{ fontWeight: "bold" }}>
                         Dr. {doctor.firstName} {doctor.lastName}
                     </Typography>
                     <Typography style={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                         {(doctor?.profile?.qualifications || []).map((qualification) => (
-                            qualification?.name 
+                            qualification?.name
                         )).join(" | ")}
-                        {<br/>}
+                        {<br />}
 
                         {(doctor?.profile?.specialities || []).map((specialitie) => (
-                            specialitie?.name 
+                            specialitie?.name
                         )).join(" | ")}
 
-                        {<br/>}
+                        {<br />}
                         {experience > 0 && `${experience} years experience`}
 
-                        
+
                     </Typography>
                     <Typography style={{ fontSize: 14 }} color="text.primary" gutterBottom>
-                        Hospital : {hospital}
-                        Languages : {(doctor?.profile?.languages||[]).map((language) => (
-                            language 
+                        Hospital : {hospital}{<br />}
+                        Languages : {(doctor?.profile?.languages || ["Not available"]).map((language) => (
+                            language
                         )).join(", ")}
+                        {<br />}
+                        Next Available :Not available
                     </Typography>
+                    </div>
+                    <Button variant="outlined" size='small' style={{borderRadius:"25px",flexGrow:0 , flexShrink:0 , bottom:"0"}}>BOOK APPOINTMENT</Button>
+                    </div>
+                    </div>
                 </CardContent>
             </Card>
         </div>
@@ -67,4 +84,3 @@ const DrCard = ({ doctor }) => {
 
 export default DrCard;
 
- 
